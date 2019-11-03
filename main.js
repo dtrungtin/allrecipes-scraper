d1d8a833048acfdb108371fb302bdfa0cc2739e2
dtrungtin <dtrungtin@gmail.com>
2019-11-03 21:05:26 +0700

    Updating

--- main.js
+++ main.js
@@ -1,130 +1,49 @@
 const Apify = require('apify');
-const _ = require('underscore');
-const safeEval = require('safe-eval');
 
 Apify.main(async () => {
-    const input = await Apify.getInput();
-    console.log('Input:');
-    console.log(input);
-
-    if (!input || !Array.isArray(input.startUrls) || input.startUrls.length === 0) {
-        throw new Error("Invalid input, it needs to contain at least one url in 'startUrls'.");
-    }
-
-    let extendOutputFunction;
-    if (typeof input.extendOutputFunction === 'string' && input.extendOutputFunction.trim() !== '') {
-        try {
-            extendOutputFunction = safeEval(input.extendOutputFunction);
-        } catch (e) {
-            throw new Error(`'extendOutputFunction' is not valid Javascript! Error: ${e}`);
-        }
-        if (typeof extendOutputFunction !== 'function') {
-            throw new Error('extendOutputFunction is not a function! Please fix it or use just default ouput!');
-        }
-    }
-
-    const dataset = await Apify.openDataset();
-    const { itemCount } = await dataset.getInfo();
-
-    let pagesOutputted = itemCount;
-
     const requestQueue = await Apify.openRequestQueue();
-
-    for (let index = 0; index < input.startUrls.length; index++) {
-        await requestQueue.addRequest({ url: input.startUrls[index].url, userData: { label: 'start' } });
-    }
+    await requestQueue.addRequest({ url: 'https://www.allrecipes.com/recipes/1031/main-dish/sandwiches/beef/', userData: { label: 'list' } });
 
     const crawler = new Apify.CheerioCrawler({
         requestQueue,
-        handlePageFunction: async ({ request, autoscaledPool, $ }) => {
-            if (request.userData.label === 'start' || request.userData.label === 'list') {
-                const paginationEle = $('.desc span');
-                if (!paginationEle || paginationEle.text() === '') {
+
+        requestTimeoutSecs: 120,
+        handlePageTimeoutSecs: 240,
+        maxConcurrency: 5,
+
+        handlePageFunction: async ({ request, $ }) => {
+            console.log(request.url);
+            if (request.userData.label === 'list') {
+                const itemLinks = $('.fixed-recipe-card > .fixed-recipe-card__info > a');
+                if (itemLinks.length === 0) {
                     return;
                 }
 
-                const content = $('.desc span').text().match(/of\s+(\d+[.,]?\d*[.,]?\d*)/)[1];
-                const pageCount = Math.floor(parseInt(content, 10) / 50); // Each page has 50 items
-
-                if (request.userData.label === 'start') {
-                    for (let index = 1; index < pageCount; index++) {
-                        const startNumber = index * 50 + 1;
-                        let startUrl = request.url;
-                        startUrl += `${startUrl.split('?')[1] ? '&' : '?'}start=${startNumber}`;
-                        await requestQueue.addRequest({ url: startUrl, userData: { label: 'list' } });
-                    }
-                }
-
-                const itemLinks = $('.lister-list .lister-item a');
                 for (let index = 0; index < itemLinks.length; index++) {
-                    const href = $(itemLinks[index]).attr('href');
-                    if (href.includes('/title/')) {
-                        const itemId = href.match(/\/title\/(.{9})/)[1];
-                        const itemUrl = `https://www.imdb.com/title/${itemId}/parentalguide`;
-
-                        await requestQueue.addRequest({ url: `${itemUrl}`, userData: { label: 'parentalguide', id: itemId } });
+                    const itemUrl = $(itemLinks[index]).attr('href');
+                    if (itemUrl) {
+                        await requestQueue.addRequest({ url: `${itemUrl}`, userData: { label: 'item' } });
                     }
                 }
-            } else if (request.userData.label === 'parentalguide') {
-                const itemCertificates = $('#certificates').text().trim();
-                const itemUrl = `https://www.imdb.com/title/${request.userData.id}`;
 
-                await requestQueue.addRequest({ url: `${itemUrl}`, userData: { label: 'item', certificates: itemCertificates } });
+                const nextPageUrl = $('link[rel=next]').attr('href');
+                console.log('nextPageUrl=' + nextPageUrl);
+                await requestQueue.addRequest({ url: `${nextPageUrl}`, userData: { label: 'list' } });
             } else if (request.userData.label === 'item') {
-                const itemTitle = $('.title_wrapper h1').text().trim();
-                const itemOriginalTitle = '';
-                const itemRuntime = $('#titleDetails div h4:contains(Runtime:)').parent().text()
-                    .replace('Runtime:', '')
-                    .trim();
-                const yearMatch = itemTitle.match(/(\d+)/);
-                const itemYear = yearMatch ? yearMatch[0] : '';
-                const itemRating = $('.ratingValue').text().trim();
-                const itemRatingCount = $('.ratingValue').text().trim();
-                const desc = $('.summary_text').text().trim();
-                const itemStars = $('.credit_summary_item h4:contains(Stars:)').parent().text()
-                    .replace('Stars:', '')
-                    .trim();
-                const itemDirector = $('.credit_summary_item h4:contains(Director:)').parent().text()
-                    .replace('Director:', '')
-                    .trim();
-                const itemGenres = $('#titleStoryLine div h4:contains(Genres:)').parent().text()
-                    .replace('Genres:', '')
-                    .trim();
-                const itemCountry = $('#titleDetails div h4:contains(Country)').parent().text()
-                    .replace('Country:', '')
-                    .trim();
-                const itemId = $('meta[property=pageId]').attr('content');
-
                 const pageResult = {
                     url: request.url,
-                    id: itemId,
-                    title: itemTitle,
-                    originalTitle: itemOriginalTitle,
-                    description: desc,
-                    genres: itemGenres,
-                    country: itemCountry,
-                    runtime: itemRuntime,
-                    rating: itemRating,
-                    ratingCount: itemRatingCount,
-                    director: itemDirector,
-                    stars: itemStars,
-                    year: itemYear,
-                    certificate: request.userData.certificates,
+                    name: $('#recipe-main-content').text(),
+                    rating: $('meta[itemprop=ratingValue]').attr('content'),
+                    ratingcount: $('meta[itemprop=reviewCount]').attr('content'),
+                    ingredients: $('.checklist').text(),
+                    prep: $('[itemprop=prepTime]').text(),
+                    cook: $('[itemprop=cookTime]').text(),
+                    ready: $('[itemprop=totalTime]').text(),
+                    Calories: $('.nutrition-summary-facts').text(),
                     '#debug': Apify.utils.createRequestDebugInfo(request),
                 };
 
-                if (extendOutputFunction) {
-                    const userResult = await extendOutputFunction($);
-                    _.extend(pageResult, userResult);
-                }
-
                 await Apify.pushData(pageResult);
-
-                if (++pagesOutputted >= input.maxItems) {
-                    const msg = `Outputted ${pagesOutputted} pages, limit is ${input.maxItems} pages`;
-                    console.log(`Shutting down the crawler: ${msg}`);
-                    autoscaledPool.abort();
-                }
             }
         },
 
@@ -134,8 +53,6 @@ Apify.main(async () => {
                 '#debug': Apify.utils.createRequestDebugInfo(request),
             });
         },
-
-        proxyConfiguration: input.proxyConfiguration,
     });
 
     await crawler.run();
